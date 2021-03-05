e11f65024f7efa5769cfa574519fbcb85f6a8b46
Sean Cobham <cobhamsean@gmail.com>
2021-03-05 11:35:06 -0800

    Backend Completed

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -14,6 +14,6 @@ const userSchema = new Schema({
     timestamps: true,
 });
 
-const User = mongoose.model('UserMern', userSchema);
+const User = mongoose.model('Users', userSchema);
 
 module.exports = User;
